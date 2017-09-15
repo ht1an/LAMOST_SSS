711ece104aac4cb7b90d311741753f0df850690e
yujc <yujc.astro@gmail.com>
2017-09-15 14:33:43 +0800

    update footprint

--- footprint/plot_footprint.py
+++ footprint/plot_footprint.py
@@ -7,8 +7,9 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from aitoff_projection import aitoff_projection
 from footprint import footprint
-from galactic import galactic
+import galactic
 
+fig, ax = plt.subplots(1,1)
 # plot coordinate
 # x, y = ra, dec
 dx = 60
@@ -18,11 +19,11 @@ while x <= 360:
     x0 = np.array([x]*len(y0))
     px0, py0 = aitoff_projection(np.deg2rad(x0), np.deg2rad(y0))
     if x == 0 or x == 360:
-        plt.plot(px0, py0, 'k-', lw=2)
+        ax.plot(px0, py0, 'k-', lw=2)
     elif x == 180:
-        plt.plot(px0, py0, 'k-', lw=1)
+        ax.plot(px0, py0, 'k-', lw=1)
     else:
-        plt.plot(px0, py0, 'k--', lw=1)
+        ax.plot(px0, py0, 'k--', lw=1)
     x = x + dx
 
 x0 = np.linspace(0.0, 360.0, 36001)
@@ -32,49 +33,49 @@ while y < 90:
     y0 = np.array([y]*len(x0))
     px0, py0 = aitoff_projection(np.deg2rad(x0), np.deg2rad(y0))
     if y == 0:
-        plt.plot(px0, py0, 'k-', linewidth=1)
+        ax.plot(px0, py0, 'k-', linewidth=1)
     else:
-        plt.plot(px0, py0, 'k--', linewidth=1)
+        ax.plot(px0, py0, 'k--', linewidth=1)
     y = y + dy
 
 # plot galactic latitude = 30, 60
-#from astropy.coordinates import SkyCoord
-#b = 0
-#db = 30
-#l0 = np.linspace(0.0, 360.0, 36001)
-#b0 = np.array([b]*len(l0))
-#x0 = np.zeros(len(l0))
-#y0 = np.zeros(len(l0))
-#for i in range(len(l0)):
-#    c = SkyCoord(l0[i], b0[i], unit='deg', frame='galactic')
-#    s1 = c.icrs.to_string('decimal')
-#    s2 = s1.split()
-#    ra = float(s2[0])
-#    dec = float(s2[1])
-#    x0[i] = ra
-#    y0[i] = dec
+b = 0
+db = 30
+l0 = np.linspace(0.0, 360.0, 36001)
+while b < 90:
+    b0 = np.array([b]*len(l0))
+#    c = SkyCoord(l0, b0, unit='deg', frame='galactic')
+#    x0 = c.icrs.ra.value
+#    y0 = c.icrs.dec.value
 
-## plot galactic latitude = 30, 60
-#b = 0
-#db = 30
-#l0 = np.linspace(0.0, 360.0, 36001)
-#while b < 90:
-#    b0 = np.array([b]*len(l0))
-#    if b == 0:
-#        ra, dec = galactic(l0, b0)
-#        idx = np.argsort(ra)
-#        x0 = ra[idx]
-#        y0 = dec[idx]
-#    else:
-#        x0, y0 = galactic(l0, b0)
-#    px0, py0 = aitoff_projection(np.deg2rad(x0), np.deg2rad(y0))
-#    plt.plot(px0, py0, 'b--', linewidth=1)
-#    b = b + db
-#
-#gp_ra = 192.859508
-#gp_dec = 27.128336
-#px1, py1 = aitoff_projection(np.deg2rad(gp_ra), np.deg2rad(gp_dec))
-#plt.scatter(px1, py1, s=2, c='b', marker='*')
+    x0, y0 = galactic.galactic2(l0, b0)
+    if b == 0:
+        ra, dec = galactic.galactic2(l0, b0)
+        idx = np.argsort(ra)
+        x0 = ra[idx]
+        y0 = dec[idx]
+    else:
+        x0, y0 = galactic.galactic2(l0, b0)
+    px0, py0 = aitoff_projection(np.deg2rad(x0), np.deg2rad(y0))
+    ax.plot(px0, py0, 'b-.', linewidth=0.5, alpha=0.5)
+    b = b + db
+
+gp_ra = 192.859508
+gp_dec = 27.128336
+px1, py1 = aitoff_projection(np.deg2rad(gp_ra), np.deg2rad(gp_dec))
+ax.scatter(px1, py1, s=4, c='b', marker='*', alpha=0.5)
+
+ax.text(-12,  -3, r'0$^{\circ}$',   fontdict={'fontsize':8})
+ax.text( 11,  45, r'30$^{\circ}$',  fontdict={'fontsize':8})
+ax.text( 76,  78, r'60$^{\circ}$',  fontdict={'fontsize':8})
+ax.text( 36,  -6, r'60$^{\circ}$',  fontdict={'fontsize':8})
+ax.text( 92,  -6, r'120$^{\circ}$', fontdict={'fontsize':8})
+ax.text(158,  -6, r'180$^{\circ}$', fontdict={'fontsize':8})
+ax.text(224,  -6, r'240$^{\circ}$', fontdict={'fontsize':8})
+ax.text(285,  -6, r'300$^{\circ}$', fontdict={'fontsize':8})
+ax.text( 7,  -51, r'-30$^{\circ}$', fontdict={'fontsize':8})
+ax.text( 72, -83, r'-60$^{\circ}$', fontdict={'fontsize':8})
+ax.text(195,  25, r'NGP', fontdict={'fontsize':8}, color='red', alpha=0.3)
 
 t = np.genfromtxt('../script/center.txt',dtype=('S19', float, float, 'S19'), names=True)
 c1 = t['ra']
@@ -97,9 +98,9 @@ for j in range(len(name2)):
         x0, y0 = footprint(cen_ra[i], cen_dec[i])
         x, y = aitoff_projection(np.deg2rad(x0), np.deg2rad(y0))
         if i == 0:
-            plt.plot(x, y, c=colors[j], ls='-', label=nx)
+            ax.plot(x, y, c=colors[j], ls='-', label=nx)
         else:
-            plt.plot(x, y, c=colors[j], ls='-')
+            ax.plot(x, y, c=colors[j], ls='-')
 
 nx = name2[k]
 idx = (name1 == nx)
@@ -107,20 +108,9 @@ cen_ra  = c1[idx]
 cen_dec = c2[idx]
 x0, y0 = footprint(cen_ra, cen_dec)
 x, y = aitoff_projection(np.deg2rad(x0), np.deg2rad(y0))
-plt.plot(x, y, c=colors[k], ls='-', label=nx)
+ax.plot(x, y, c=colors[k], ls='-', label=nx)
+ax.legend(loc=1, bbox_to_anchor=(1.06, 1.06), fontsize=8, fancybox=True, framealpha=0.5)
 
-#plt.plot(x, y, 'r-', label='ChenLi')
-#
-#for i in range(len(t)):
-#    cen_ra = ca[i]
-#    cen_dec = cd[i]
-#    x0, y0 = footprint(cen_ra, cen_dec)
-#    x, y = aitoff_projection(np.deg2rad(x0), np.deg2rad(y0))
-#    if i == 0:
-#        plt.plot(x, y, 'b-', label='T-Taurus')
-#    else:
-#        plt.plot(x, y, 'b-')
-
-plt.legend(loc='best', fontsize=8, fancybox=True, framealpha=0.5)
+ax.set_title('LAMOST-MRS test footprint')
 
 plt.savefig('fp.pdf', format='pdf')
